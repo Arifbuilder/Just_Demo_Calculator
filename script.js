5e128eb8ca894ca8a3ab1dd20e9408fbb22a54df
Arifbuilder <ashikarifashik8@gmail.com>
2026-07-04 22:02:09 +0530

    Merge remote and local histories

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function clickbutton(val) {
-    document.getElementById("screen").value+= val;
+    document.getElementById("screen").value += val;
 }
 
 function clearDisplay() {
     document.getElementById("screen").value = "";
-   
 }
-function equalClick(){
-    var text=document.getElementById("screen").value
-    var result=eval(text);
-    document.getElementById("screen").value=result;
 
+function equalClick() {
+    var text = document.getElementById("screen").value;
+    try {
+        var result = eval(text);
+        if (result === undefined || isNaN(result) && text !== "") {
+            document.getElementById("screen").value = "Error";
+        } else {
+            document.getElementById("screen").value = result;
+        }
+    } catch (error) {
+        document.getElementById("screen").value = "Error";
+    }
 }
- 
